d02029d14699a860971e029b5c1695aa52979060
= <=>
2023-01-14 17:09:53 +0200

    v1.4

--- src/context/BasketContext.js
+++ src/context/BasketContext.js
@@ -12,7 +12,7 @@ export const BasketProvider = (props) => {
         if (cards.length > 0) {
             setIsEmpty(false)
         }
-        if (cards.length === 1) {
+        if (basketCounter === 1) {
             setIsShowPanel(true);
         }
         counter()
